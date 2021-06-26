8682f32c61edad44a4ac2f409e8ee55204842282
Joilton07 <joilton@aluno.fapce.edu.br>
2021-06-26 09:55:54 -0300

    adicionando biblioteca cors

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,15 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import {router} from "./routes";
 import "./database";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 
 app.use(router);
+
+app.use(cors());
 
 app.use(
     (err: Error, request: Request, response: Response, next: NextFunction) => {
